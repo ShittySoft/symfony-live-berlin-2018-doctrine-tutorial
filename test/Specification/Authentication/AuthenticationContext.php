a5a8a5913f6276f38ae22544ca4be2e1e0bc572e
Marco Pivetta <ocramius@gmail.com>
2018-05-01 23:19:38 +0200

    Added test assets and specification for the project (just empty files, for now)

---
+++ test/Specification/Authentication/AuthenticationContext.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Test\Specification\Authentication;
+
+use Behat\Behat\Context\Context;
+
+final class AuthenticationContext implements Context
+{
+}
---
+++ test/Specification/Blog/BlogPostCommentsContext.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Test\Specification\Blog;
+
+use Behat\Behat\Context\Context;
+
+final class BlogPostCommentsContext implements Context
+{
+}
