f961c9d8298c4933fbcfd9596a09b57edc18a69e
Marco Pivetta <ocramius@gmail.com>
2018-10-24 12:01:04 +0200

    Example value type for an `EmailAddress`

---
+++ src/Authentication/Value/EmailAddress.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Authentication\Value;
+
+final class EmailAddress
+{
+    /** @var string */
+    private $email;
+
+    private function __construct(string $email)
+    {
+        $this->email = $email;
+    }
+
+    public static function fromEmailAddress(string $emailAddress) : self
+    {
+        if (! filter_var($emailAddress, \FILTER_VALIDATE_EMAIL)) {
+            throw new \InvalidArgumentException(sprintf(
+                '%s is not a valid %s',
+                $emailAddress,
+                self::class
+            ));
+        }
+
+        return new self($emailAddress);
+    }
+
+    public function toString() : string
+    {
+        return $this->email;
+    }
+}
